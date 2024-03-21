0a674cbffed4d501eb9e0d926a4a80f9eb3c527a
houseplayer <houseplayer@gmail.com>
2024-03-21 16:20:11 +0100

    refactor

--- main.js
+++ main.js
@@ -9,22 +9,22 @@ const WIDTH = 320;
 const HEIGHT = 550;
 
 let grid;
-let cellSize = 10;
+let cellSize = 5;
 let rows = WIDTH / cellSize;
 let columns = HEIGHT / cellSize;
 let hue = 1;
 
-const makeLookoutTable = (rows, cols) => {
+function makeLookoutTable(rows, cols) {
   let arr = new Array(rows);
+
   for (let i = 0; i < arr.length; i++) {
     arr[i] = new Array(cols);
     for (let j = 0; j < arr[i].length; j++) {
       arr[i][j] = 0;
     }
   }
-
   return arr;
-};
+}
 
 function setup() {
   const cnv = createCanvas(WIDTH, HEIGHT);
@@ -40,15 +40,25 @@ function setup() {
 }
 
 function mouseDragged() {
-  let i = floor(mouseX / cellSize);
-  let j = floor(mouseY / cellSize);
-  if (i >= 0 && i < rows && j >= 0 && j < columns && grid[i][j] === 0) {
-    grid[i][j] = hue;
-  }
-  if (hue >= 360) {
-    hue = 1;
-  } else {
-    hue += 0.2;
+  let mouseHorizontal = floor(mouseX / cellSize);
+  let mouseVertical = floor(mouseY / cellSize);
+  let paintWidth = 5;
+
+  for (let i = -floor(paintWidth / 2); i < floor(paintWidth / 2); i++) {
+    for (let j = -floor(paintWidth / 2); j < floor(paintWidth / 2); j++) {
+      let fullMouseHorizontal = mouseHorizontal + i;
+      let fullMouseVertical = mouseVertical + j;
+
+      if (
+        fullMouseHorizontal >= 0 &&
+        fullMouseHorizontal < rows &&
+        grid[fullMouseHorizontal][fullMouseVertical] === 0
+      ) {
+        grid[fullMouseHorizontal][fullMouseVertical] = hue;
+      }
+    }
+
+    hue = hue >= 360 ? 1 : hue + 0.2;
   }
 }
 
@@ -74,19 +84,15 @@ function draw() {
     for (let j = 0; j < columns; j++) {
       if (grid[i][j] > 0) {
         const randomDirection = random([-1, 1]);
+        const dumpCell =
+          i - randomDirection >= 0 &&
+          i - randomDirection <= rows - 1 &&
+          grid[i - randomDirection][j + 1];
 
-        if (j < columns - 1) {
-          if (grid[i][j + 1] > 0) {
-            next[i][j] = grid[i][j];
-
-            if (i > 0 && i < rows - 1 && grid[i - randomDirection][j + 1] === 0) {
-              next[i - randomDirection][j] = grid[i][j];
-              next[i][j] = 0;
-            }
-          } else {
-            next[i][j] = 0;
-            next[i][j + 1] = grid[i][j];
-          }
+        if (grid[i][j + 1] === 0) {
+          next[i][j + 1] = grid[i][j];
+        } else if (dumpCell === 0) {
+          next[i - randomDirection][j + 1] = grid[i][j];
         } else {
           next[i][j] = grid[i][j];
         }
